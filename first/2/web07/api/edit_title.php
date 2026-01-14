24f61bf248f518202e068622cdce9fe1127732f0
christophe-feng <fengxup@gmail.com>
2026-01-14 15:00:45 +0800

    修正乙級第一題 edit_title.php 內容

--- first/2/web07/api/edit_title.php
+++ first/2/web07/api/edit_title.php
@@ -1,3 +1,4 @@
+<!-- 編輯back/title.php資料 -->
 <?php
 include_once "db.php";
 
@@ -5,7 +6,7 @@ foreach($_POST['text'] as $id=>$text){
     if(!empty($_POST['del']) && in_array($id,$_POST['del'])){
         $Title->del($id);
     }else{
-        $row=$Tite->find($id);
+        $row=$Title->find($id);
         $row['text']=$text;
         $row['sh']=(isset($_POST['sh']) && $_POST['sh']==$id)?1:0;
 
