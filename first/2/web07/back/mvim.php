ccc9e66b3f6da4924e034695da1f0ba796af6815
ChristopheLin <fengxup@gmail>
2026-01-18 15:24:41 +0800

    簡化後台編輯功能

--- first/2/web07/back/mvim.php
+++ first/2/web07/back/mvim.php
@@ -1,6 +1,6 @@
 <div style="width:99%; height:87%; margin:auto; overflow:auto; border:#666 1px solid;">
     <p class="t cent botli">動畫圖片管理</p>
-    <form method="post" action="./api/edit_mvim.php">
+    <form method="post" action="./api/edit.php?table=<?= $do; ?>">
         <table width="100%">
             <tbody>
                 <tr class="yel">
@@ -15,17 +15,17 @@
                 ?>
                     <tr>
                         <td width="68%">
-                            <img src="./upload/<?= $row['img']; ?>" style="width:180px;height:120px">
+                            <img src="./upload/<?= $row['img']; ?>" style="width:180px;height:120px;">
                             <input type="hidden" name="id[]" value="<?= $row['id']; ?>">
                         </td>
                         <td width="7%">
-                            <input type="checkbox" name="sh[]" value="<?= $row['id']; ?>" id="" <?= ($row['sh'] == 1) ? "checked" : "" ?>>
+                            <input type="checkbox" name="sh[]" value="<?= $row['id']; ?>"  <?= ($row['sh'] == 1) ? "checked" : "" ?>>
                         </td>
                         <td width="7%">
                             <input type="checkbox" name="del[]" value="<?= $row['id']; ?>">
                         </td>
                         <td>
-                            <input type="button" value="更換動畫" onclick="op('#cover','#cvr','./modal/update_<?= $do; ?>.php?id=<?= $row['id']; ?>')">
+                            <input type="button" value="更換動畫" onclick="op('#cover','#cvr','./modal/update.php?table=<?= $do; ?>&id=<?= $row['id']; ?>')">
                         </td>
                     </tr>
                 <?php
