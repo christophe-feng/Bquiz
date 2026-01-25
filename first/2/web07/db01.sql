0ea1b00bdf4e8a656b9de0121b03065197dcee4d
ChristopheLin <fengxup@gmail>
2026-01-26 01:44:03 +0800

    更新乙級第一題資料庫

--- first/2/web07/db01.sql
+++ first/2/web07/db01.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主機： 127.0.0.1
--- 產生時間： 2026-01-19 16:59:39
+-- 產生時間： 2026-01-25 18:42:40
 -- 伺服器版本： 10.4.32-MariaDB
 -- PHP 版本： 8.2.12
 
@@ -38,10 +38,11 @@ CREATE TABLE `ad` (
 --
 
 INSERT INTO `ad` (`id`, `text`, `sh`) VALUES
-(1, '1 轉知臺北教育大學與臺灣師大合辦第11屆麋研齋全國硬筆書法比賽活動', 1),
-(2, '2 轉知:法務部辦理「第五屆法規知識王網路闖關競賽辦法', 0),
-(4, '4 欣榮圖書館101年悅讀達人徵文比賽，歡迎全校師生踴躍投稿參加', 0),
-(6, '轉知2012年全國青年水墨創作大賽活動', 1);
+(1, '轉知臺北教育大學與臺灣師大合辦第11屆麋研齋全國硬筆書法比賽活動', 1),
+(2, '轉知:法務部辦理「第五屆法規知識王網路闖關競賽辦法', 0),
+(4, '欣榮圖書館101年悅讀達人徵文比賽，歡迎全校師生踴躍投稿參加', 0),
+(6, '轉知2012年全國青年水墨創作大賽活動', 1),
+(7, '轉知:教育是人類升沉的樞紐-2013教師生命成長營', 1);
 
 -- --------------------------------------------------------
 
@@ -67,6 +68,24 @@ INSERT INTO `admin` (`id`, `acc`, `pw`) VALUES
 -- --------------------------------------------------------
 
 --
+-- 資料表結構 `bottom`
+--
+
+CREATE TABLE `bottom` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `bottom` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 傾印資料表的資料 `bottom`
+--
+
+INSERT INTO `bottom` (`id`, `bottom`) VALUES
+(1, '2026 頁尾版權');
+
+-- --------------------------------------------------------
+
+--
 -- 資料表結構 `image`
 --
 
@@ -83,7 +102,7 @@ CREATE TABLE `image` (
 INSERT INTO `image` (`id`, `img`, `sh`) VALUES
 (1, '01D01.jpg', 1),
 (4, '01D03.jpg', 1),
-(7, '01D06.jpg', 0),
+(7, '01D06.jpg', 1),
 (8, '01D07.jpg', 1),
 (9, '01D02.jpg', 1);
 
@@ -108,7 +127,9 @@ CREATE TABLE `menu` (
 INSERT INTO `menu` (`id`, `href`, `text`, `main_id`, `sh`) VALUES
 (1, '?do=login', '管理登入', 0, 1),
 (2, 'index.php', '網站首頁', 0, 1),
-(4, 'dgvdgsd', 'ghhgdrghdfsgs', 0, 0);
+(4, 'dgvdgsd', 'ghhgdrghdfsgs', 0, 0),
+(5, 'aaaa11', '1111a354', 4, 0),
+(7, 'cccc33', '3333cczzz', 4, 0);
 
 -- --------------------------------------------------------
 
@@ -129,8 +150,8 @@ CREATE TABLE `mvim` (
 INSERT INTO `mvim` (`id`, `img`, `sh`) VALUES
 (1, '01C01.gif', 1),
 (2, '01C02.gif', 1),
-(4, '01C05.gif', 0),
-(6, '01C02.gif', 0),
+(4, '01C05.gif', 1),
+(6, '01C02.gif', 1),
 (7, '01C04.gif', 1);
 
 -- --------------------------------------------------------
@@ -151,8 +172,12 @@ CREATE TABLE `news` (
 
 INSERT INTO `news` (`id`, `text`, `sh`) VALUES
 (2, '公告綜合高中一年級英數補救教學時間\r\n上課日期:10/27.11/3.11/10.11/24共計四次\r\n上課時間:早上8:00~11:50半天\r\n費用:全程免費\r\n參加同學:綜合科一年級第一次段考成績需加強者\r\n已將名單送交各班及導師\r\n參加同學請帶紙筆.課本.第一次段考考卷\r\n並將家長通知單給家長\r\n若有任何疑問\r\n請洽綜合高中學程主任', 1),
-(3, '102年全國大專校院運動會\r\n「主題標語及吉祥物命名」\r\n網路票選活動\r\n一、活動期間：自10月25日起至11月4日止。\r\n二、相關訊息請上宜蘭大學首頁連結「102全大運在宜大」\r\n活動網址：http://102niag.niu.edu.tw/', 1),
-(4, '台灣亞洲藝術文化教育交流學會第一屆年會國際研討會\r\n活動日期：101年3月3～4日(六、日)\r\n活動主題：創造力、文化、全人教育\r\n有意參加者請至http://www.caaetaiwan.org下載報名表\r\n', 1);
+(3, '102年全國大專校院運動會\r\n「主題標語及吉祥物命名」\r\n網路票選活動\r\n一、活動期間：自10月25日起至11月4日止。\r\n二、相關訊息請上宜蘭大學首頁連結「102全大運在宜大」\r\n活動網址：http://102niag.niu.edu.tw/', 0),
+(4, '台灣亞洲藝術文化教育交流學會第一屆年會國際研討會\r\n活動日期：101年3月3～4日(六、日)\r\n活動主題：創造力、文化、全人教育\r\n有意參加者請至http://www.caaetaiwan.org下載報名表\r\n', 0),
+(5, '台視百萬大明星節目辦理海選活動\r\n時間:101年10月27日下午13時\r\n地點:彰化\r\n', 1),
+(6, '財團法人漢光教育基金會\r\n辦理2012「舊愛新歡-古典詩詞譜曲創作暨歌唱表演競賽」\r\n參賽獎金豐厚!!', 1),
+(7, '11月23日(星期五)將於彰化縣田尾鄉菁芳園休閒農場\r\n舉辦「高中職生涯輔導知能研習」\r\n中區學校每校至多2名\r\n以普通科、專業類科教師優先報名參加\r\n生涯規劃教師次之，參加人員公差假\r\n並核實派代課\r\n當天還有專車接送(8:35前在員林火車站集合)\r\n如此好康的機會，怎能錯過？！\r\n熱烈邀請師長們向輔導室(分機234)報名\r\n名額有限，動作要快！！\r\n報名截止日期：本周四 10月25日17:00前！', 0),
+(8, '教師研習「世界公民生命園丁國內研習會」\r\n1.主辦單位：世界展望會\r\n2.研習日期：101年11月14日（三）～15日（四）\r\n3.詳情請參考：\r\nhttp://gc.worldvision.org.tw/seed.html。\r\n請線上報名。\r\n', 1);
 
 -- --------------------------------------------------------
 
@@ -172,10 +197,28 @@ CREATE TABLE `title` (
 --
 
 INSERT INTO `title` (`id`, `img`, `text`, `sh`) VALUES
-(3, '01B04.jpg', 'fsrgg', 0),
-(4, '01B04.jpg', 'dgvsdfs', 1),
+(3, '01B04.jpg', '卓越科技大學校園資訊系統', 1),
+(4, '01B04.jpg', 'dgvsdfs', 0),
 (5, '01B03.jpg', '135135', 0),
 (8, '01B02.jpg', '1536415', 0);
+
+-- --------------------------------------------------------
+
+--
+-- 資料表結構 `total`
+--
+
+CREATE TABLE `total` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `total` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- 傾印資料表的資料 `total`
+--
+
+INSERT INTO `total` (`id`, `total`) VALUES
+(1, 1);
 
 --
 -- 已傾印資料表的索引
@@ -191,6 +234,12 @@ ALTER TABLE `ad`
 -- 資料表索引 `admin`
 --
 ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 資料表索引 `bottom`
+--
+ALTER TABLE `bottom`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -224,6 +273,12 @@ ALTER TABLE `title`
   ADD PRIMARY KEY (`id`);
 
 --
+-- 資料表索引 `total`
+--
+ALTER TABLE `total`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- 在傾印的資料表使用自動遞增(AUTO_INCREMENT)
 --
 
@@ -231,13 +286,19 @@ ALTER TABLE `title`
 -- 使用資料表自動遞增(AUTO_INCREMENT) `ad`
 --
 ALTER TABLE `ad`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- 使用資料表自動遞增(AUTO_INCREMENT) `admin`
 --
 ALTER TABLE `admin`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `bottom`
+--
+ALTER TABLE `bottom`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- 使用資料表自動遞增(AUTO_INCREMENT) `image`
@@ -249,7 +310,7 @@ ALTER TABLE `image`
 -- 使用資料表自動遞增(AUTO_INCREMENT) `menu`
 --
 ALTER TABLE `menu`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- 使用資料表自動遞增(AUTO_INCREMENT) `mvim`
@@ -261,13 +322,19 @@ ALTER TABLE `mvim`
 -- 使用資料表自動遞增(AUTO_INCREMENT) `news`
 --
 ALTER TABLE `news`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- 使用資料表自動遞增(AUTO_INCREMENT) `title`
 --
 ALTER TABLE `title`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- 使用資料表自動遞增(AUTO_INCREMENT) `total`
+--
+ALTER TABLE `total`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
