5209d30ed2be31ad97fa23af6ce828a6e68c13d6
christophe-feng <fengxup@gmail.com>
2026-01-30 09:03:40 +0800

    新增今日上課內容：根據訂單狀況計算各場次的剩餘票數

--- third/1/web07/api/get_sessions.php
+++ third/1/web07/api/get_sessions.php
@@ -1,29 +1,23 @@
 <?php
 include_once "db.php";
 
-$movieId=$_GET['movieId'];
-$date=strtotime($_GET['date']);
+$movieId = $_GET['movieId'];
+$movie = $Movie->find($movieId)['name'];    //取得電影名稱
+$date = strtotime($_GET['date']);
 
-$duration=[
-    1=>"14:00 ~ 16:00",
-    2=>"16:00 ~ 18:00",
-    3=>"18:00 ~ 20:00",
-    4=>"20:00 ~ 22:00",
-    5=>"22:00 ~ 24:00"
-];
-
-$today=strtotime(date("Y-m-d"));
-$start=1;
-if($date==$today){
-    $H=date("G");
-    if($H>=14){
-        $start=ceil(($H-13)/2)+1;
+$today = strtotime(date("Y-m-d"));
+$start = 1;
+if ($date == $today) {
+    $H = date("G");
+    if ($H >= 14) {
+        $start = ceil(($H - 13) / 2) + 1;
     }
 }
 
-for($i=1;$i<=5;$i++){
-    echo "<option value='$i'>{$duration[$i]} 剩餘座位 20</option>";
-    }
+for ($i = 1; $i <= 5; $i++) {
+    // $sql = "SELECT sum(`qt`) as 'qt' FROM `orders` WHERE movie='$movie' && date='{$_GET['date']}' && session='{$duration[$i]}'";
+    // $qt = 20 - q($sql)[0]['qt'];
 
-
-?>
+    $qt = 20 - $Order->sum('qt'," WHERE movie='$movie' && date='{$_GET['date']}' && session='{$duration[$i]}'");
+    echo "<option value='$i'>{$duration[$i]} 剩餘座位 $qt</option>";
+}
