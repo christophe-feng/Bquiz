8eff7ee27c5b7617a0612ec690fe8cfa72231e09
weir <wei080781@gmail.com1>
2026-02-23 16:23:21 +0800

    乙級第四題商品詳細資訊建置及購物車練習

--- fourth/2/web04/front/main.php
+++ fourth/2/web04/front/main.php
@@ -10,7 +10,7 @@ if (isset($_GET['type']) && $_GET['type'] != 0) {
     } else {
         $big = $Type->find($type['big_id']);
         $nav_str = $big['name'] . " > " . $type['name'];
-        $rows = $Item->all(['sh' => 1, 'big' => $type['id']]);
+        $rows = $Item->all(['sh' => 1, 'mid' => $type['id']]);
     }
 } else {
     $nav_str = "全部商品";
@@ -25,17 +25,21 @@ foreach ($rows as $row):
 ?>
     <!-- .pp(.pp.ct>img)+(div>.ct.tt+.pp*3) -->
     <div class="pp" style="display: flex;width:70%;margin:2px auto">
-        <div class="pp ct" >
-            <img src="upload/<?= $row['img']; ?>" style="width:150px;height:120px">
+        <div class="pp ct" style="width:40%;padding:10px;border:1px solid white">
+            <a href="?do=detail&id=<?= $row['id']; ?>">
+                <img src="upload/<?= $row['img']; ?>" style="width:150px;height:120px">
+            </a>
         </div>
         <div style="width: 60%">
-            <div class="ct tt" style="border:1px solid white"><?= $row['name']; ?></div>
-            <div class="pp">
+            <div class="ct tt" style="padding:5px 2px;border:1px solid white"><?= $row['name']; ?></div>
+            <div class="pp" style="padding:5px 2px;border:1px solid white">
                 價錢：<?= $row['price']; ?>
-                <img src="icon/0402.jpg" alt="">
+                <a href="?do=buycart&id=<?= $row['id']; ?>&qt=1" style="float: right;">
+                    <img src="icon/0402.jpg" alt="">
+                </a>
             </div>
-            <div class="pp">規格：<?= $row['spec']; ?></div>
-            <div class="pp">簡介：<?= $row['intro']; ?></div>
+            <div class="pp" style="padding:5px 2px;border:1px solid white">規格：<?= $row['spec']; ?></div>
+            <div class="pp" style="padding:5px 2px">簡介：<?= mb_substr($row['intro'], 0, 20); ?>...</div>
         </div>
     </div>
 <?php
