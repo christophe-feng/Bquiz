3648965dd41ca3495490fc695d221dda97b2c22d
ChristopheLin <fengxup@gmail>
2026-03-04 02:34:14 +0800

    乙級第二題第三次練習：後台最新消息頁面建置練習、更新資料庫

--- second/3/web02/back/news.php
+++ second/3/web02/back/news.php
@@ -1,1 +1,33 @@
-最新文章管理
+<form action="" method="post">
+    <table class="ct" style="margin:auto;width:80%">
+        <tr>
+            <td style="width:10%;">編號</td>
+            <td style="width:70%;">標題</td>
+            <td>顯示</td>
+            <td>刪除</td>
+        </tr>
+        <?php
+        $rows=$Post->all();
+        foreach($rows as $row):
+        ?>
+        <tr>
+            <td class="clo"></td>
+            <td><?= $row['title']; ?></td>
+            <td>
+                <input type="checkbox" name="sh[]" value="<?= $row['id']; ?>" <?= ($row['sh']==1)?'checked':''; ?>>
+            </td>
+            <td>
+                <input type="checkbox" name="del[]" value="<?= $row['id']; ?>">
+                <!-- 要放入隱藏的id以利將資料送到後端做處理 -->
+                <input type="hidden" name="id[]" value="<?= $row['id']; ?>">
+            </td>
+        </tr>
+        <?php
+        endforeach;
+        ?>
+    </table>
+    <div class="ct">
+        <!-- 用submit即可 -->
+        <input type="submit" value="確定修改">
+    </div>
+</form>
